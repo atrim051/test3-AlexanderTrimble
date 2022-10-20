d0ddd1a20046d5e3a8001b42c82cc4eb981bb552
atrim109 <atrim051@gmail.com>
2022-10-20 13:36:56 -0400

    new

---
+++ data_prep.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { resolve } = require('path');
+let students = [];
+
+module.exports.prep = () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile('./data/students.json', (err, data) => {
+        if (err) {
+          reject("unable to read file");
+        }
+        students = JSON.parse(data);
+        resolve();
+      });
+    });
+  };
+  
+  module.exports.cpa = function () {
+    return new Promise((resolve, reject) => {
+      if (students.length == 0) {
+        reject('No Results Returned');
+      }
+      resolve(students);
+    });
+  };
+  
+  module.exports.highGPA = function () {
+    return new Promise((resolve, reject) => {
+        let highestGPA;
+        if (students.length == 0) {
+          reject('No Results Returned');
+        }
+        for (let i = 0; i < students.length; i++){
+            for (let j = i + 1; j < students.length; j++) {
+                if (students[j].gpa > students[i].gpa){
+                    highestGPA = students[j];
+                }
+            }
+        }
+        resolve(highestGPA);
+      });
+    };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*************************************************************************
-* WEB322– Test 3
+* WEB322– Test 2
 * I declare that this assignment is my own work in accordance with Seneca Academic
 Policy. No part * of this assignment has been copied manually or electronically from any
 other source
@@ -19,49 +19,48 @@ var data = require("./data_prep.js");
 
 function onHTTPStart() {
     console.log('Express http server listening on: ' + HTTP_PORT);
-  };
+  }
 
-app.listen(HTTP_PORT, onHTTPStart);
 
 // setup a 'route' to listen on the default url path
-// app.get("/", (req, res) => {
-//     var resTxt = `<h2>Declaration</h2><p>I acknowledge the College's academic integrity policy - and my own integrity - remain in 
-//     effect whether my work is done remotely or onsite. Any test or assignmnet is an act of trust between me and my instructor, and 
-//     especially with my classmates... even when no one is watching. I declare I will not break that trust </p>
-//     <p>Name: <mark>Alexander Trimble</mark></p>
-//     <p>Student Number: <mark>144365160</mark>
-//     <p><a href=/CPA>Click to vist CPA Students</a></p>
-//     <p><a href=/highGPA>Click to see who has the highest GPA</a></p>`
-//     res.send(resTxt);
-// });
+app.get("/", (req, res) => {
+    var resTxt = `<h2>Declaration</h2><p>I acknowledge the College's academic integrity policy - and my own integrity - remain in 
+    effect whether my work is done remotely or onsite. Any test or assignmnet is an act of trust between me and my instructor, and 
+    especially with my classmates... even when no one is watching. I declare I will not break that trust </p>
+    <p>Name: <mark>Alexander Trimble</mark></p>
+    <p>Student Number: <mark>144365160</mark>
+    <p><a href=/CPA>Click to vist CPA Students</a></p>
+    <p><a href=/highGPA>Click to see who has the highest GPA</a></p>`
+    res.send(resTxt);
+});
 
-// app.get("/CPA", (req, res) => {
-//     data
-//       .cpa()
-//       .then((data) => {
-//         res.json(data);
-//       })
-//       .catch((err) => {
-//         res.json("ERROR");
-//       });
-// });
+app.get("/CPA", (req, res) => {
+    data
+      .cpa()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json("ERROR");
+      });
+});
 
-// app.get('/highGPA', (req, res) => {
-//     data
-//       .highGPA()
-//       .then((data) => {
-//         res.json(data);
-//       })
-//       .catch((err) => {
-//         res.json("ERROR");
-//       });
-//   });
+app.get('/highGPA', (req, res) => {
+    data
+      .highGPA()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json("ERROR");
+      });
+  });
 
-// data
-//   .prep()
-//   .then(function () {
-//     app.listen(HTTP_PORT, onHTTPStart);
-//   })
-//   .catch(function (err) {
-//     console.log('No Data. Failed to start.' + err);
-//   });
+data
+  .prep()
+  .then(function () {
+    app.listen(HTTP_PORT, onHTTPStart);
+  })
+  .catch(function (err) {
+    console.log('No Data. Failed to start.' + err);
+  });
