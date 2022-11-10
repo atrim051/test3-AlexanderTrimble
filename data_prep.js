2fb0fb0ec7bf735dfd21286a64063dceb0b796e7
atrim109 <atrim051@gmail.com>
2022-11-10 14:27:59 -0500

    new

--- data_prep.js
+++ data_prep.js
@@ -1,175 +1,68 @@
-var fs = require("fs");
+const fs = require('fs');
+const { resolve } = require('path');
+let students = [];
 
-var students=[];
-
-exports.prep = ()=>{
-
-   // console.log("Testing");
-
-   return new Promise((resolve, reject)=>{
-
-        fs.readFile("./students.json", (err, data)=>{
-
-            if (err) {reject("unable to read file.");}
-
-            //students = JSON.parse(data);
-
-           // console.log(students);
-
-            resolve("File read success.");
-
-        }); 
-
-   });
-
+module.exports.prep = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/students.json', (err, data) => {
+      if (err) {
+        reject("unable to read file");
+      }
+      students = JSON.parse(data);
+      resolve();
+    });
+  });
 };
 
+module.exports.cpa = function () {
+  return new Promise((resolve, reject) => {
+    let results = students.filter(student => student.program == "CPA");
+    (results.length == 0) ? reject("No CPA students.") : resolve(results);
+  });
+};
 
-
-exports.bsd = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-       let results = students.filter(student => student.program == "BSD");
-
-       (results.length == 0)? reject("No BSD students."):resolve(results);
-
-    });
-
-}
-
-
-
-
-
-exports.cpa = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-       let results = students.filter(student => student.program == "CPA");
-
-       (results.length == 0)? reject("No CPA students."):resolve(results);
-
-    });
-
-}
-
-exports.highGPA = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        let high = 0;
-
-        let highStudent;
-
-        
-
-        for (let i=0; i<students.length; i++)
-
+module.exports.highGPA = function () {
+  return new Promise((resolve, reject)=>{
+    let high = 0;
+    let highStudent;
+    
+    for (let i=0; i<students.length; i++)
+    {
+        //console.log(students[i].gpa, high);
+        if (students[i].gpa > high)
         {
-
-            //console.log(students[i].gpa, high);
-
-            if (students[i].gpa > high)
-
-            {
-
-                high = students[i].gpa;
-
-                highStudent = students[i];
-
-            }
-
+            high = students[i].gpa;
+            highStudent = students[i];
         }
-
-        (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
-
-    }); 
-
+    }
+    (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
+}); 
 };
 
-
-
-exports.lowGPA = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        let low = 4.0;
-
-        let lowStudent;
-
-        for (let i=0; i<students.length; i++)
-
-        {
-
-            if (students[i].gpa < low)
-
-            {
-
-                low = students[i].gpa;
-
-                lowStudent = students[i];
-
-            }
-
-        }
-
-        resolve(lowStudent);
-
-    }); 
-
+module.exports.allStudents = () => {
+  return new Promise((resolve, reject) => {
+    if (students.length == 0) {
+      reject('No Results Returned');
+    }
+    resolve(students);
+  });
 };
 
-
-
-exports.allStudents =()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        if (students.length>0)
-
-        {
-
-            resolve(students);
-
-        } else reject("No students.");
-
-    })
-
-}
-
-
-
-exports.addStudent= (stud)=>{
-
-    return new Promise((resolve, reject)=>{
-
-        stud.studId = students.length+1;
-
-        students.push(stud);
-
-        resolve();
-
+module.exports.addStudent = function (studentData) {
+  return new Promise((resolve, reject) => {
+    students.push(studentData);
+    resolve(
+      studentData
+    );
     });
+  };
 
-}
-
-
-
-exports.getStudent = (studId)=>{
-
-    return new Promise((resolve, reject)=>{
-
-        students.forEach(function(student){
-
-            if (student.studId == studId)
-
-                resolve(student);
-
-        });
-
-        reject("No result found!");
-
-    })
-
-}
+  module.exports.getStudent = (num) => {
+    return new Promise((resolve, reject) => {
+      var stuByID = students.filter((student) =>{
+        return student.studId == num; 
+      });
+      if (stuByID.length == 0) reject('No Student by this status found!');
+      else resolve(stuByID);
+    });
+  };
