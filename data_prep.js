64e73106ad7ed62f2f2e7560b23750943208ab1c
atrim109 <atrim051@gmail.com>
2022-11-10 14:33:16 -0500

    new

--- data_prep.js
+++ data_prep.js
@@ -15,28 +15,77 @@ module.exports.prep = () => {
 };
 
 module.exports.cpa = function () {
-  return new Promise((resolve, reject) => {
-    let results = students.filter(student => student.program == "CPA");
-    (results.length == 0) ? reject("No CPA students.") : resolve(results);
-  });
-};
+    return new Promise((resolve, reject)=>{
+
+        let results = students.filter(student => student.program == "CPA");
+ 
+        (results.length == 0)? reject("No CPA students."):resolve(results);
+ 
+     });
+ 
+ }
 
 module.exports.highGPA = function () {
-  return new Promise((resolve, reject)=>{
-    let high = 0;
-    let highStudent;
-    
-    for (let i=0; i<students.length; i++)
-    {
-        //console.log(students[i].gpa, high);
-        if (students[i].gpa > high)
+    return new Promise((resolve, reject)=>{
+
+        let high = 0;
+
+        let highStudent;
+
+        
+
+        for (let i=0; i<students.length; i++)
+
         {
-            high = students[i].gpa;
-            highStudent = students[i];
+
+            //console.log(students[i].gpa, high);
+
+            if (students[i].gpa > high)
+
+            {
+
+                high = students[i].gpa;
+
+                highStudent = students[i];
+
+            }
+
         }
-    }
-    (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
-}); 
+
+        (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
+
+    }); 
+
+
+};
+
+module.exports.lowGPA = function () {
+    return new Promise((resolve, reject)=>{
+
+        let low = 4.0;
+
+        let lowStudent;
+
+        for (let i=0; i<students.length; i++)
+
+        {
+
+            if (students[i].gpa < low)
+
+            {
+
+                low = students[i].gpa;
+
+                lowStudent = students[i];
+
+            }
+
+        }
+
+        resolve(lowStudent);
+
+    }); 
+
 };
 
 module.exports.allStudents = () => {
@@ -48,21 +97,33 @@ module.exports.allStudents = () => {
   });
 };
 
-module.exports.addStudent = function (studentData) {
-  return new Promise((resolve, reject) => {
-    students.push(studentData);
-    resolve(
-      studentData
-    );
+module.exports.addStudent = (stud)=>{
+
+    return new Promise((resolve, reject)=>{
+
+        stud.studId = students.length+1;
+
+        students.push(stud);
+
+        resolve();
+
     });
-  };
+
+}
 
   module.exports.getStudent = (num) => {
-    return new Promise((resolve, reject) => {
-      var stuByID = students.filter((student) =>{
-        return student.studId == num; 
-      });
-      if (stuByID.length == 0) reject('No Student by this status found!');
-      else resolve(stuByID);
-    });
-  };
+    return new Promise((resolve, reject)=>{
+
+        students.forEach(function(student){
+
+            if (student.studId == studId)
+
+                resolve(student);
+
+        });
+
+        reject("No result found!");
+
+    })
+
+}
